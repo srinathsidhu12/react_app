c41686f1aa04c204ff26c140cbe5be41a7ea01d3
root <root@ip-172-31-42-158.ap-south-1.compute.internal>
2025-08-05 09:14:47 +0000

    react js app files added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Hello from React App!</h1>
+      <p>This is a sample app for Jenkins deployment.</p>
+    </div>
+  );
+}
+
+export default App;
+
